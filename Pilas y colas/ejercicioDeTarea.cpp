0ffc8227bb43a36caa4bca9ffe227a5bf0901dab
lauraippolito28 <159057618+lauraippolito28@users.noreply.github.com>
2024-05-03 19:39:22 -0500

    Update ejercicioDeTarea.cpp

--- Pilas y colas/ejercicioDeTarea.cpp
+++ Pilas y colas/ejercicioDeTarea.cpp
@@ -1,3 +1,30 @@
-/*
-LISTA DE CAMCIONES DE VIDEOS FAVORITOS DE YOUTUBE Y DECRI SI ES PILA O COLA. 
+/* LISTA DE CAMCIONES DE VIDEOS FAVORITOS DE YOUTUBE Y DECRI SI ES PILA O COLA. 
 CREAR UN EJERCICIO: SIMULAR LOS TURNOS QUE SE PIDE EN UNA EPS PARA ACCEDER A UNA CITA MÉDICA Y DETERMINAR SI ES PILA O COLA.*/
+
+
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int main() {
+    stack<string> FavoritosYoutube;
+
+    // Agregar canciones a la lista
+    FavoritosYoutube.push("Hookiao");
+    FavoritosYoutube.push("Your Song");
+    FavoritosYoutube.push("Saturno");
+    FavoritosYoutube.push("Un mundo ideal");
+
+    // Mostrar la lista de favoritos
+    cout << "Canciones de favoritos en YouTube:" << endl;
+    while (!FavoritosYoutube.empty()) {
+        cout << FavoritosYoutube.top() << endl;
+        FavoritosYoutube.pop();
+    }
+
+    return 0;
+}
+
+/*ESTE CÓDIGO USÓ PILAS*/
